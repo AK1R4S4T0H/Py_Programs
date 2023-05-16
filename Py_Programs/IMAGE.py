8f586ea469f2b993d2870fb2f6872b0ed1834652
AK1R4S4T0H <deavers.chadwick@gmail.com>
2023-05-16 14:08:58 -0400

    update IMAGE.py

--- Py_Programs/IMAGE.py
+++ Py_Programs/IMAGE.py
@@ -2,7 +2,7 @@
 # need image to rezise to screen not the other way around
 #
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import ttk, filedialog
 from PIL import Image, ImageTk
 import os
 
@@ -15,16 +15,21 @@ class ImageViewer:
         self.images = []
         self.current_image = 0
 
+        self.style = ttk.Style()
+        self.style.configure("TButton", background="#555555", foreground="white")
+        self.style.map('TButton', foreground=[("hover", "white")], background=[("hover", "#660066")])
+        self.style.configure("TLabel", background="#220022", foreground="white")
+
         self.canvas = tk.Canvas(self.master, bg='#220022', borderwidth=15, width=400, height=400)
         self.canvas.pack(side="top", fill="both", expand=True)
 
-        self.previous_button = tk.Button(self.master, bg='grey', fg='white', text="Previous", command=self.show_previous_image)
+        self.previous_button = ttk.Button(self.master, style="TButton", text="Previous", command=self.show_previous_image)
         self.previous_button.pack(side="left", padx=10, pady=10)
 
-        self.next_button = tk.Button(self.master, bg='grey', fg='white', text="Next", command=self.show_next_image)
+        self.next_button = ttk.Button(self.master, style="TButton", text="Next", command=self.show_next_image)
         self.next_button.pack(side="right", padx=10, pady=10)
 
-        self.browse_button = tk.Button(self.master, bg='grey', fg='white', text="Browse", command=self.browse_directory)
+        self.browse_button = ttk.Button(self.master,style="TButton", text="Browse", command=self.browse_directory)
         self.browse_button.pack(side="bottom", ipadx=25, pady=10)
 
     def browse_directory(self):
@@ -38,7 +43,27 @@ class ImageViewer:
         for file in os.listdir(directory):
             if file.endswith(".jpg") or file.endswith(".png"):
                 image = Image.open(os.path.join(directory, file))
+                image = self.resize_image(image)
                 self.images.append(ImageTk.PhotoImage(image))
+
+    def resize_image(self, image):
+        canvas_width = self.canvas.winfo_width()
+        canvas_height = self.canvas.winfo_height()
+        image_width, image_height = image.size
+
+        # Calculate the aspect ratio
+        width_ratio = canvas_width / image_width
+        height_ratio = canvas_height / image_height
+
+        # Choose the smaller ratio to maintain the aspect ratio of the image
+        ratio = min(width_ratio, height_ratio)
+
+        # Calculate the new size
+        new_width = int(image_width * ratio)
+        new_height = int(image_height * ratio)
+
+        # Resize the image
+        return image.resize((new_width, new_height), Image.ANTIALIAS)
 
     def show_image(self):
         self.canvas.delete("all")
