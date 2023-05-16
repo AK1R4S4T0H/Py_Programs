227f81f7d37a1ba0cf15aa0778e2b7bd3979b7d0
AK1R4S4T0H <deavers.chadwick@gmail.com>
2023-05-16 14:19:25 -0400

    updates

--- Py_Programs/VIDEO.py
+++ Py_Programs/VIDEO.py
@@ -1,7 +1,7 @@
 # mp4 video player using cv2 and tkinter
 #
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import ttk, filedialog
 import cv2
 from PIL import Image, ImageTk
 
@@ -11,20 +11,25 @@ class VideoPlayer:
         self.video_path = ""
         self.cap = None
 
-        self.canvas = tk.Canvas(root)
-        self.canvas.pack()
+        self.style = ttk.Style()
+        self.style.configure("TButton", background="#555555", foreground="white")
+        self.style.map('TButton', foreground=[("hover", "white")], background=[("hover", "#660066")])
 
-        self.btn_open = tk.Button(root, text="Open Video", command=self.open_video)
-        self.btn_open.pack()
+        self.canvas = tk.Canvas(root, bg='#220022', borderwidth=15, width=700, height=700)
+        self.canvas.pack(side="top", fill="both", expand=True)
+        self.canvas.bind("<Configure>", self.on_canvas_resize)
 
-        self.btn_play = tk.Button(root, text="Play", command=self.play_video)
-        self.btn_play.pack()
+        self.btn_open = ttk.Button(root, text="Open Video", command=self.open_video, style="TButton")
+        self.btn_open.pack(side="top", fill="both", expand=True)
 
-        self.btn_pause = tk.Button(root, text="Pause", command=self.pause_video)
-        self.btn_pause.pack()
+        self.btn_play = ttk.Button(root, text="Play", command=self.play_video, style="TButton")
+        self.btn_play.pack(side="right", fill="both", expand=True)
 
-        self.btn_stop = tk.Button(root, text="Stop", command=self.stop_video)
-        self.btn_stop.pack()
+        self.btn_pause = ttk.Button(root, text="Pause", command=self.pause_video, style="TButton")
+        self.btn_pause.pack(side="left", fill="both", expand=True)
+
+        self.btn_stop = ttk.Button(root, text="Stop", command=self.stop_video, style="TButton")
+        self.btn_stop.pack(side="bottom", fill="both", expand=True)
 
     def open_video(self):
         self.video_path = filedialog.askopenfilename(filetypes=(("Video Files", "*.mp4"), ("All Files", "*.*")))
@@ -36,12 +41,19 @@ class VideoPlayer:
         if self.cap is not None:
             ret, frame = self.cap.read()
             if ret:
+                # Resize the frame to fit the canvas size
+                canvas_width = self.canvas.winfo_width()
+                canvas_height = self.canvas.winfo_height()
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 image = Image.fromarray(image)
+                image = image.resize((canvas_width, canvas_height), Image.ANTIALIAS)
                 image = ImageTk.PhotoImage(image)
                 self.canvas.create_image(0, 0, anchor=tk.NW, image=image)
                 self.canvas.image = image
-                self.root.after(30, self.play_video)  # Delay between frames
+        self.root.after(30, self.play_video)  # Delay between frames
+
+    def on_canvas_resize(self, event):
+        self.play_video()
 
     def pause_video(self):
         # pause the video by simply not calling play_video
