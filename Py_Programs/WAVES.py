e339e90a2df39d21dd98ca62a2ffbbd44c05c5b0
AK1R4S4T0H <deavers.chadwick@gmail.com>
2023-05-19 00:35:02 -0400

    better

--- Py_Programs/WAVES.py
+++ Py_Programs/WAVES.py
@@ -8,9 +8,9 @@ import random
 from scipy.fft import fft
 
 # Audio settings
-CHANNELS = 3
+CHANNELS = 7
 SAMPLE_RATE = 44100
-BLOCK_SIZE = 1024
+BLOCK_SIZE = 2048
 
 # Visualization settings
 SCREEN_WIDTH = 800
@@ -19,7 +19,7 @@ BACKGROUND_COLOR = (0, 0, 0)
 NUM_WAVEFORMS = 7
 WAVEFORM_COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (255, 255, 255)]
 LINE_WIDTH = 2
-WAVEFORM_MOVEMENT = 17000  # Adjust the value to control the pronounced movement of the waveform
+WAVEFORM_MOVEMENT = 99000  # Adjust the value to control the pronounced movement of the waveform
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -28,16 +28,16 @@ pygame.display.set_caption("Waveform Visualizer")
 
 def audio_capture_callback(indata, frames, time, status):
     audio_data = indata.mean(axis=1)
-    audio_data = np.interp(np.linspace(0, len(audio_data) - 2, SCREEN_WIDTH), np.arange(len(audio_data)), audio_data)
-    scaled_data = audio_data * (SCREEN_HEIGHT / 15) + (SCREEN_HEIGHT / 15)
+    audio_data = np.interp(np.linspace(0, len(audio_data) - 1/2, SCREEN_WIDTH), np.arange(len(audio_data)), audio_data)
+    scaled_data = audio_data * (SCREEN_HEIGHT / 16) + (SCREEN_HEIGHT / 16)
     screen.fill(BACKGROUND_COLOR)
 
-    waveforms = [scaled_data * (i + 40) / NUM_WAVEFORMS for i in range(NUM_WAVEFORMS)]
+    waveforms = [scaled_data * (i + 1/2) / NUM_WAVEFORMS for i in range(NUM_WAVEFORMS)]
 
     waveform_height = SCREEN_HEIGHT / NUM_WAVEFORMS
     for i, waveform in enumerate(waveforms):
-        y_offset = int(i * waveform_height + waveform_height / 2)
-        scaled_waveform = waveform * (i + 1) / NUM_WAVEFORMS  # Adjust the scaling factor
+        y_offset = int(i * waveform_height + waveform_height / 50)
+        scaled_waveform = waveform * (i + 1/2) / NUM_WAVEFORMS  # Adjust the scaling factor
         waveform_points = np.column_stack((np.arange(SCREEN_WIDTH), scaled_waveform + y_offset)).astype(int)
         pygame.draw.lines(screen, WAVEFORM_COLORS[i], False, waveform_points, LINE_WIDTH)
 
